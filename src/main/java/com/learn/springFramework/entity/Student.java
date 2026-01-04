44925b0775c4be256bb478bbe4c5501786a69554
Streen Jiang <streen.jiang@outlook.com>
2026-01-04 18:26:36 +0800

    IOC/DI: When some fields are Lists

--- src/main/java/com/learn/springFramework/entity/Student.java
+++ src/main/java/com/learn/springFramework/entity/Student.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -11,5 +13,5 @@ public class Student {
     private long id;
     private String name;
     private int age;
-    private Address address;
+    private List<Address> addresses;
 }
