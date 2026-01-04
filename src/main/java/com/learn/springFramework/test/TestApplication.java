4059bfb3cf70bbde275a8c25aaf0b34e240584f8
Streen Jiang <streen.jiang@outlook.com>
2026-01-04 18:30:31 +0800

    IOD/DI: Example for singleton scope and prototype

--- src/main/java/com/learn/springFramework/test/TestApplication.java
+++ src/main/java/com/learn/springFramework/test/TestApplication.java
@@ -10,5 +10,22 @@ public class TestApplication {
 
         Student student = (Student) context.getBean("student");
         System.out.println("student: " + student);
+
+        Student student1 = (Student) context.getBean("student");
+        System.out.println("student1: " + student1);
+
+        System.out.println("==================");
+        System.out.println("They are the same student: " + (student == student1));
+        System.out.println("==================");
+
+        Student student2 = (Student) context.getBean("student2");
+        System.out.println("student2: " + student2);
+
+        Student student2_2 = (Student) context.getBean("student2");
+        System.out.println("student2_2: " + student2_2);
+
+        System.out.println("==================");
+        System.out.println("They are the same student: " + (student2 == student2_2));
+        System.out.println("==================");
     }
 }
